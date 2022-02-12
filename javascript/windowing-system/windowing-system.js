5ab9cfe689d35bf01fd3da091c1128feca7d470b
calebjoshuapaul <calebjoshuapaul@gmail.com>
2022-02-12 23:37:28 +0530

    update

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -12,8 +12,8 @@ export class Size {
     }
 
     resize(newWidth, newHeight) {
-        this.width = newWidth
-        this.height = newHeight
+            this.width = newWidth
+            this.height = newHeight
     }
 }
 
@@ -32,6 +32,41 @@ export class Position {
 
 export class ProgramWindow {
     screenSize = new Size(800, 600)
-    size = new Size
-    position = new Position
+    size = new Size()
+    position = new Position()
+
+    resize(newSize) {
+        let width = newSize.width <= 0 ? 1 
+        : (newSize.width + this.position.x <= this.screenSize.width) 
+        ? newSize.width 
+        : this.screenSize.width - this.position.x;
+
+        let height = newSize.height <= 0 ? 1 
+        : (newSize.height + this.position.y <= this.screenSize.height) 
+        ? newSize.height 
+        : this.screenSize.height - this.position.y;
+        
+            this.size.resize(width, height);
+    }
+
+    move(newPosition) {
+        let x = newPosition.x < 1 ? 0 
+        : (newPosition.x + this.size.width <= this.screenSize.width) 
+        ? newPosition.x 
+        : this.screenSize.width - this.size.width;
+
+        let y = newPosition.y < 1 ? 0 
+        : (newPosition.y + this.size.height <= this.screenSize.height) 
+        ? newPosition.y 
+        : this.screenSize.height - this.size.height;
+
+
+            this.position.move(x, y);
+    }
+}
+
+export function changeWindow(programWindow) {
+    programWindow.size.resize(400, 300);
+    programWindow.position.move(100,150);
+    return programWindow;
 }
